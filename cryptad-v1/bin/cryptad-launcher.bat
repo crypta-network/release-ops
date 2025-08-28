ed999f1239960ef39d09babe02940822034641ce
Leumor <116955025+leumor@users.noreply.github.com>
2025-08-29 00:03:47 +0100

    repo: remove committed cryptad-v1 payload and ignore future cryptad-v* directories

--- cryptad-v1/bin/cryptad-launcher.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal enableextensions
-set DIR=%~dp0
-set CP=%DIR%..\\lib\\*
-set JAVA_EXE=%JAVA_HOME%\\bin\\java.exe
-if not exist "%JAVA_EXE%" set JAVA_EXE=java
-"%JAVA_EXE%" -cp "%CP%" network.crypta.launcher.LauncherKt %*
